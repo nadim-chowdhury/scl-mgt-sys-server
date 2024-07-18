c509f130c1f76d7ba81c4ee6959faa961895c033
shaheen2013 <coredeveloper.2013@gmail.com>
2024-07-18 11:23:29 +0600

    code updated

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,13 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       process.env.LOCAL_URL,
+      process.env.LOCAL_URL2,
       process.env.CLIENT_URL,
       process.env.SERVER_URL,
     ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders:
+      'Content-Type,Authorization,x-apollo-operation-name,apollo-require-preflight',
     credentials: false,
   });
 
