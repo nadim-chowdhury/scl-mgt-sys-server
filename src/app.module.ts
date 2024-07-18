c1240d146f2cd53cbde5266f2035dc1ec6926110
nadim-chowdhury <nadim-chowdhury@outlook.com>
2024-07-18 14:16:55 +0600

    code updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -53,7 +53,7 @@ import { VirtualClassModule } from './virtual-class/virtual-class.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      csrfPrevention: true,
+      csrfPrevention: false,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
